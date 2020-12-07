3cbf2a49df11626451a2ac8ba4b0a599bc92a6a5
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-12-07 16:43:10 -0300

    Code refactor

--- index.js
+++ index.js
@@ -10,78 +10,62 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-let loading = false;
-let base64Url;
+let base64Url = null;
 let connClient = null;
-
-
-open("http://localhost:4000")
-
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-ioLocal.on('connection', socketLocal => {
-  createVenom();
-
-  function createVenom(){
-    venom
+const init = () => {
+  ioLocal.emit("loading")
+  venom
     .create('session', (base64Qr) => {
-      console.log('chegou até aqui no venom create')
-      loading = true;
+      ioLocal.emit("loadingFalse")
       base64Url = base64Qr;
-
-      socketLocal.emit("paring", base64Url);
+      ioLocal.emit("paring", base64Url);
     }, 
     (statusSession, session) => {
       if (statusSession === 'qrReadSuccess') {
-        console.log('escaneado')
-        socketLocal.emit('success')
+        ioLocal.emit('success')
+        socket.emit('successOnConnect')
       }
     }, 
-    {logQR: true, autoClose: 0})
-    .then((client) => {
-      socket.emit('join', {
-        name: 'cliente',
-        room: 'vendergas'
-      });
+    {logQR: false, autoClose: 0})
+    .then(client => {
       connClient = client;
-      socketLocal.emit("isAuthenticated");
+      ioLocal.emit("isAuthenticated");
       start(client)
     })
     .catch((erro) => {
       console.log(erro);
     });
-  }
+}
+init();
 
-    
-  function start(client) {
+function kill(){
+  console.log('killed')
+  rimraf("tokens", () => {
+    connClient.close();
+  })
+  init()
+}
+  
+function start(client) {
+  socket.emit('join', {
+    name: 'cliente',
+    room: 'vendergas'
+  });
+  client.onMessage(async msg => {
+    if (msg.from !== 'status@broadcast') {
+      if (msg.isMedia == true || msg.isMMS == true) {
+        try {
+          const buffer = await client.decryptFile(msg); 
+          let base64data = Buffer.from(buffer).toString('base64');
+          let imagebase64 = 'data:'+msg.mimetype+';base64,'+base64data;
 
-    client.onMessage(async msg => {
-      if (msg.from !== 'status@broadcast') {
-        if (msg.isMedia == true || msg.isMMS == true) {
-          try {
-            const buffer = await client.decryptFile(msg); 
-            let base64data = Buffer.from(buffer).toString('base64');
-            let imagebase64 = 'data:'+msg.mimetype+';base64,'+base64data;
-
-            socket.emit('moduleSend', {
-              msgContent: imagebase64,
-              type: msg.type,
-              from: msg.from,
-              to: msg.to,
-              name: msg.sender.pushname ? msg.sender.pushname : msg.from,
-              avatar: msg.sender.profilePicThumbObj.img ? msg.sender.profilePicThumbObj.img : 'none',
-              timestamp: msg.timestamp 
-            });
-          } catch (err) {
-            console.error(err);
-            console.log('Ocorreu um erro ao realizar decode64 do arquivo');
-          }    
-        }else{
           socket.emit('moduleSend', {
-            msgContent: msg.body,
+            msgContent: imagebase64,
             type: msg.type,
             from: msg.from,
             to: msg.to,
@@ -89,73 +73,69 @@ ioLocal.on('connection', socketLocal => {
             avatar: msg.sender.profilePicThumbObj.img ? msg.sender.profilePicThumbObj.img : 'none',
             timestamp: msg.timestamp 
           });
-        }
-      } 
-    })
-
-    // Retrieve all unread message
-    //const messages = await client.getAllUnreadMessages();
-
-    client.onStateChange(state => {
-      if ('CONFLICT'.includes(state)) {
-        kill()
-        socket.emit('errorOnConnect')
+        } catch (err) {
+          console.error(err);
+          console.log('Ocorreu um erro ao realizar decode64 do arquivo');
+        }    
+      }else{
+        socket.emit('moduleSend', {
+          msgContent: msg.body,
+          type: msg.type,
+          from: msg.from,
+          to: msg.to,
+          name: msg.sender.pushname ? msg.sender.pushname : msg.from,
+          avatar: msg.sender.profilePicThumbObj.img ? msg.sender.profilePicThumbObj.img : 'none',
+          timestamp: msg.timestamp 
+        });
       }
-      if ('UNPAIRED'.includes(state)) {
-        kill()
-        socket.emit('errorOnConnect')
-      }
-      if('CONNECTED'.includes(state)) {
-        socket.emit('successOnConnect')
-      }
-    })
+    } 
+  })
+  
+  client.onStateChange(state => {
+    if ('CONFLICT'.includes(state)) {
+      kill()
+      socket.emit('errorOnConnect')
+    }
+    if ('UNPAIRED'.includes(state)) {
+      kill()
+      socket.emit('errorOnConnect')
+    }
+    if('CONNECTED'.includes(state)) {
+      socket.emit('successOnConnect')
+    }
+  })
 
-    socket.on('frontReceive', async data => {
-      await client.sendSeen(data.to);
-      client.sendText(data.to, String(data.msgContent));
-    })
+  socket.on('frontReceive', async data => {
+    await client.sendSeen(data.to);
+    client.sendText(data.to, String(data.msgContent));
+  })
 
-    socket.on('markAsRead', async data => {
-      await client.sendSeen(data);
-    })
+  socket.on('markAsRead', async data => {
+    await client.sendSeen(data);
+  })
 
-    socket.on("oldMessages", async () => {
-      const chats = await client.getAllChats();
-      const device = await client.getHostDevice();
-      const allUnreadMessages = await client.getAllUnreadMessages();
-
-      allUnreadMessages.map(async (unreadMessage, i) => {
-
-        if(unreadMessage.isMedia || unreadMessage.isMMS){
-          const buffer = await client.decryptFile(unreadMessage); 
-          let base64data = Buffer.from(buffer).toString('base64');
-          let imagebase64 = 'data:'+unreadMessage.mimetype+';base64,'+base64data;
-          
-          allUnreadMessages[i].body = imagebase64;
-        }
+  socket.on("oldMessages", async () => {
+    const chats = await client.getAllChats();
+    const device = await client.getHostDevice();
+    const allUnreadMessages = await client.getAllUnreadMessages();
+    allUnreadMessages.map(async (unreadMessage, i) => {
+      if(unreadMessage.isMedia || unreadMessage.isMMS){
+        const buffer = await client.decryptFile(unreadMessage); 
+        let base64data = Buffer.from(buffer).toString('base64');
+        let imagebase64 = 'data:'+unreadMessage.mimetype+';base64,'+base64data;
         
-      })
+        allUnreadMessages[i].body = imagebase64;
+      }
+    })    
+    socket.emit('listenOldMessages', {clients: chats, hostDevice: device, unreadMessages : allUnreadMessages});
+  })
+}
 
-      // var unreadMessages = [];
 
-      // console.log(allUnreadMessages.length);
-
-      // allUnreadMessages.map(unreadMessage => {
-      //   if(unreadMessage.invis == false)
-      //     unreadMessages.push(unreadMessage);
-      // })
-
-      
-      socket.emit('listenOldMessages', {clients: chats, hostDevice: device, unreadMessages : allUnreadMessages});
-    })
-  }
-
-  function kill(){
-    rimraf("tokens", () => {connClient.close(); createVenom()})
-  }
-
-  socketLocal.on('closeConnection', async () => {
-    await kill();
+ioLocal.on('connection', clientLocal => {
+  clientLocal.on('closeConnection', () => {
+    kill()
+    socket.emit('errorOnConnect')
   })
 })
 
@@ -163,5 +143,6 @@ ioLocal.on('connection', socketLocal => {
 
 
 http.listen(4000, () => {
+  open("http://localhost:4000")
   console.log('listening on *:4000');
 });
